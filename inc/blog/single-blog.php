3cf77e620356bc92b0dbf9c8398334378239d9be
Dinesh Chouhan <dineshc@bsf.io>
2018-01-15 12:26:10 +0530

    Mircrodata for front page issue fixed

--- inc/blog/single-blog.php
+++ inc/blog/single-blog.php
@@ -55,11 +55,11 @@ if ( ! function_exists( 'astra_single_post_class' ) ) {
 		// Blog layout.
 		if ( is_singular() ) {
 			$classes[] = 'ast-article-single';
-
-			// Remove hentry from page.
-			if ( 'page' == get_post_type() ) {
-				$classes = array_diff( $classes, array( 'hentry' ) );
-			}
+		}
+		
+		// Remove hentry from page.
+		if ( 'page' == get_post_type() || is_home() ) {
+			$classes = array_diff( $classes, array( 'hentry' ) );
 		}
 
 		return $classes;
