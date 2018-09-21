c2eba19176d81956a20c128a13090f2ffdeee537
Nikhil <email@nikhilchavan.com>
2018-09-21 15:54:24 +0530

    phpcs code style fixes

--- inc/blog/blog-config.php
+++ inc/blog/blog-config.php
@@ -30,7 +30,8 @@ if ( ! function_exists( 'astra_get_post_meta' ) ) {
 			switch ( $meta_value ) {
 
 				case 'author':
-					if ( ! empty( get_the_author() ) ) {
+					$author = get_the_author();
+					if ( ! empty( $author ) ) {
 						$output_str .= ( 1 != $loop_count && '' != $output_str ) ? ' ' . $separator . ' ' : '';
 						$output_str .= esc_html( astra_default_strings( 'string-blog-meta-author-by', false ) ) . astra_post_author();
 					}
@@ -132,8 +133,10 @@ if ( ! function_exists( 'astra_post_author' ) ) {
 		?>
 
 		<span class="posted-by vcard author" itemtype="https://schema.org/Person" itemscope="itemscope" itemprop="author">
-			<a class="url fn n" title="<?php printf( __( 'View all posts by %1$s', 'astra' ), esc_attr( get_the_author() ) ); ?>" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author" itemprop="url">
-				<span class="author-name" itemprop="name"><?php esc_html_e( get_the_author() ); ?></span>
+			<?php // Translators: Author Name. ?>
+			<a class="url fn n" title="<?php printf( __( 'View all posts by %1$s', 'astra' ), get_the_author() ); ?>" 
+				href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author" itemprop="url">
+				<span class="author-name" itemprop="name"><?php echo get_the_author(); ?></span>
 			</a>
 		</span>
 
